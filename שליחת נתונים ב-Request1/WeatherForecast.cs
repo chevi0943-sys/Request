59b96de37bfb68dd0ad9583b0219df883276b236
chevi <chevi0943@gmail.com>
2025-12-09 23:27:59 +0200

    first commit

---
+++ שליחת נתונים ב-Request1/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace שליחת_נתונים_ב_Request1
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
